784cf45710c82ff6f4d00e8276c8cfb2df703858
Kristina <you@example.com>
2025-10-21 15:39:38 +0700

    Add template inheritance with base.html

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
-    path("", views.index),  # главная страница
-    path("about/", views.about),  # страница "О блоге"
+    path("", views.index),
+    path("about/", views.about),
     path("articles/", include("hexlet_django_blog.article.urls")),
     path("admin/", admin.site.urls),
 ]
